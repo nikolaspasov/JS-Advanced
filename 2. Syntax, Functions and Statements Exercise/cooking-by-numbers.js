a4ff1bd7b1d17d3846e3f6c304592d021b9dc4f6
nikolaspasov <belinelli100@gmail.com>
2021-06-07 13:47:38 +0300

    add new files

---
+++ 2. Syntax, Functions and Statements Exercise/cooking-by-numbers.js
@@ -0,0 +1,44 @@
+function operate(number,op1,op2,op3,op4,op5){
+
+    switch(op1){
+        case'chop':number = number/2; break;
+        case'dice': number = Math.sqrt(number); break;
+        case'spice':number++; break;
+        case'bake':number=number*3 ;break;
+        case'fillet':number=number*0.80 ;break;
+    }
+    console.log(number);
+    switch(op2){
+        case'chop':number = number/2; break;
+        case'dice':number = Math.sqrt(number); break;
+        case'spice':number++; break;
+        case'bake':number=number*3 ;break;
+        case'fillet':number=number*0.80 ;break;
+    }
+    console.log(number);
+    switch(op3){
+        case'chop':number = number/2; break;
+        case'dice':number =Math.sqrt(number); break;
+        case'spice':number++; break;
+        case'bake':number=number*3 ;break;
+        case'fillet':number=number*0.80 ;break;
+    }
+    console.log(number);
+    switch(op4){
+        case'chop':number = number/2; break;
+        case'dice':number = Math.sqrt(number); break;
+        case'spice':number++; break;
+        case'bake':number=number*3 ;break;
+        case'fillet':number=number*0.80 ;break;
+    }
+    console.log(number);
+    switch(op5){
+        case'chop':number = number/2; break;
+        case'dice':number = Math.sqrt(number); break;
+        case'spice':number++; break;
+        case'bake':number=number*3 ;break;
+        case'fillet':number=number*0.80 ;break;
+    }
+    console.log(number);
+}
+ 
