3aa506c662239612d7bed67ddf3e2ee799ffa5c0
nikolaspasov <belinelli100@gmail.com>
2021-06-09 18:08:07 +0300

    add new files

---
+++ 3. Arrays/negative-positive-numbers.js
@@ -0,0 +1,24 @@
+function print(arr){
+
+    let newArr=[];
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index]<0){
+           newArr.unshift(arr[index]);
+        }
+        
+    }
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index]>=0){
+            newArr.push(arr[index]);
+        }
+        
+    }
+
+    for (let index = 0; index < newArr.length; index++) {
+       
+        console.log(newArr[index]); 
+
+    }
+
+
+}
---
+++ 3. Arrays/smallest-two-numbers.js
@@ -0,0 +1,6 @@
+function print(arr){
+
+
+}
+
+print([30, 15, 50, 5]);
