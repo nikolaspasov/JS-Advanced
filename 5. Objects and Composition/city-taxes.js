6bb8a21f343b1b91b6f3176b34871e8339e60c65
nikolaspasov <belinelli100@gmail.com>
2021-06-17 16:24:16 +0300

    add new file

--- 5. Objects and Composition/city-taxes.js
+++ 5. Objects and Composition/city-taxes.js
@@ -1,17 +1,24 @@
 function cityTaxes(name,population,treasury){
 
-    let city ={};
+    return{
     
-    let taxRate = 10;
-
-    city.name = name;
-    city.population = population;
-    city.treasury=treasury;
-
-
-     
     
+    name,
+    population,
+    treasury,
+    taxRate: 10,
+
+    collectTaxes:function() {
+        this.treasury += this.population*this.taxRate;
+    },
+
+    applyGrowth(percent){
+        this.population+=Math.floor(this.population*(percent/100));
+    },
+
+    applyRecession(percentage){
+        this.treasury-=Math.floor(this.treasury*(percent/100));
+    },
+};
+  
 }
-
-const city = cityTaxes('Tortuga', 7000, 15000);
-console.log(city);
