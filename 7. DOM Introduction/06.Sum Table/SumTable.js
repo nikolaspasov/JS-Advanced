7aeb48068d3b84b27c918cd53648561d914ec4b5
nikolaspasov <belinelli100@gmail.com>
2021-07-10 15:05:15 +0300

    add new files

--- 7. DOM Introduction/06.Sum Table/SumTable.js
+++ 7. DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,25 @@
 function sumTable() {
 
+
+    let tableElements = document.querySelectorAll('td');
+
+    let sum=0;
+
+    
+    for (let element of tableElements) {
+        
+        let currElement = Number(element.textContent)
+
+        if(!isNaN(currElement))
+        {
+            sum+=currElement;
+ 
+        }
+    }
+    
+    document.querySelector('#sum').textContent=sum;
+   
+
+
+    
 }
