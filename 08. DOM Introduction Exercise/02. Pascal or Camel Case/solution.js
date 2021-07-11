df68481477011e56c854025cd09bffc7d444d805
nikolaspasov <belinelli100@gmail.com>
2021-07-11 18:11:00 +0300

    add file changes

--- 08. DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
+++ 08. DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  
+
+  let input = document.getElementById('text').value;
+  let caseType = document.getElementById('naming-convention').value;
+
+  let editedText;
+  console.log(input);
+  console.log(caseType);
+ switch (caseType) {
+   
+   case 'Camel Case': 
+   editedText= input.toLowerCase();
+   document.getElementById('result').textContent=editedText;
+   break;
+
+   case 'Pascal Case':
+   editedText= input.toLowerCase();
+   document.getElementById('result').textContent=editedText;
+   break;
+   
+   default:
+     
+    document.getElementById('result').textContent='Error!';
+ }
+ 
+
+
 }
