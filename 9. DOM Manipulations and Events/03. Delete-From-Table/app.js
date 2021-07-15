3a9a01aa91d7a4be7af40357018a6221e519e8ac
nikolaspasov <belinelli100@gmail.com>
2021-07-15 11:37:58 +0300

    add new files

--- 9. DOM Manipulations and Events/03. Delete-From-Table/app.js
+++ 9. DOM Manipulations and Events/03. Delete-From-Table/app.js
@@ -1,3 +1,30 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    
+
+
+    let emailInputElement = document.querySelector("input[name='email']").value;
+    
+    let membersNodeList = document.querySelectorAll("tbody>tr");
+
+    let membersListElement = Array.from(membersNodeList);
+
+    let foundExistingMember = false;
+    
+
+
+    membersListElement.forEach(member => {
+        
+        if(member.textContent.includes(emailInputElement)){
+            member.remove();
+            let resultElement = document.getElementById('result');
+            resultElement.textContent='Deleted';
+            foundExistingMember=true;
+        }
+        
+    });
+    if(foundExistingMember==false)
+    {
+        let resultElement = document.getElementById('result');
+        resultElement.textContent='Not found.';
+    }
 }
