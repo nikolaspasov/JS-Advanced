61eb5bacdf082a399109e9e62272997b509e2c1d
nikolaspasov <belinelli100@gmail.com>
2021-07-16 19:08:27 +0300

    add new files

--- 9. DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ 9. DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    
+    let inputFieldElement = document.getElementById('email');
+
+    var validRegex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+    inputFieldElement.addEventListener('change', function checkEmail(){
+
+       if(inputFieldElement.value.match(validRegex)){
+
+        inputFieldElement.setAttribute('class','');
+       }
+       else{
+           
+        inputFieldElement.setAttribute('class','error');
+       } 
+    })
 }
