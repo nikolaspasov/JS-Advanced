6703be67b0d9e2c3af0d7198d5be81da0bfb0896
nikolaspasov <belinelli100@gmail.com>
2021-07-16 18:06:43 +0300

    add new files

--- 9. DOM Manipulations and Events/05. Highlight-Active/app.js
+++ 9. DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,5 +1,30 @@
 function focused() {
     
+    let sectionElementsNodeList = document.querySelectorAll('body div div input');
+
+    let sectionElements = Array.from(sectionElementsNodeList);
 
     
+    sectionElements.forEach(element=>{
+   
+        element.addEventListener('focus',function onFocusSection(ev){
+
+            let inputParentElement = element.parentElement;
+            inputParentElement.setAttribute('class','focused')
+        });
+
+        element.addEventListener('blur',function onBlurSection(ev) {
+
+            let inputParentElement = element.parentElement;
+            inputParentElement.removeAttribute('class');
+        });
+    
+});
+   
+
+
+    
+
+
 }
+
