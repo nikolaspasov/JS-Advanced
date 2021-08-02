055e7800a6101e6147da1150842d89734a716c58
nikolaspasov <belinelli100@gmail.com>
2021-08-02 15:06:17 +0300

    add new files

---
+++ 15-classes/point-distance.js
@@ -0,0 +1,30 @@
+class Point{
+
+
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+
+    }
+    static distance(p1,p2){
+
+    let xDistance = Math.abs(p2.x - p1.x);
+    let yDistance = Math.abs(p2.y - p1.y);
+
+    return Math.sqrt(Math.pow(xDistance,2)+Math.pow(yDistance,2));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
+
